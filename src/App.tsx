87bf88d9072fdd47b1dc7046ebb02c3a415062d2
Javid <javid.momeni@gmail.com>
2024-02-14 15:35:12 +0330

    build

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import './App.css'
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import PageOne from './onboarding/PageOne';
 import PageTwo from './onboarding/PageTwo';
-
+import WebApp from '@twa-dev/sdk'
 function App() {
+  WebApp.ready();
   return (
     <Router>
       <Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import WebApp from '@twa-dev/sdk'
+
 import { clarity } from 'react-microsoft-clarity'
-WebApp.ready();
+
 clarity.init("kzhxxy2ip7");
+
+
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
